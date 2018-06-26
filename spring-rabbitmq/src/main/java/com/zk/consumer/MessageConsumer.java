c21c7facaafdbd65b4701b36003e02cef42aef74
zhukai <zhukaixlyx@sina.com>
2018-06-26 18:14:01 +0800

    spring+rabbitmq整合

---
+++ spring-rabbitmq/src/main/java/com/zk/consumer/MessageConsumer.java
@@ -0,0 +1,18 @@
+package com.zk.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+
+public class MessageConsumer implements MessageListener {
+
+  private Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
+
+  @Override
+  public void onMessage(Message message) {
+    logger.info("consumer receive message------->:{}", message);
+
+  }
+
+}
---
+++ spring-rabbitmq/src/main/java/com/zk/consumer/ShijjConsumer.java
@@ -0,0 +1,16 @@
+package com.zk.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+
+public class ShijjConsumer implements MessageListener {
+
+  private Logger logger = LoggerFactory.getLogger(ShijjConsumer.class);
+
+  @Override
+  public void onMessage(Message message) {
+    logger.info("shijj receive message------->:{}", message);
+  }
+}
